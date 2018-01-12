1304f1b126fc9a7ae4686d13140bcee11ed397f2
Thomas Jang <tom@axisj.com>
2018-01-12 17:47:16 +0900

    webpack production

--- webpack.production.js
+++ webpack.production.js
@@ -42,34 +42,18 @@ module.exports = {
         test: /\.css$/,
         use: [
           {loader: 'style-loader'},
-          {loader: 'css-loader'}
+          {loader: 'css-loader', options: {
+
+          } }
         ]
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-      },
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
     ],
   },
   plugins: [
