ad6047671926177a85ca09ed008291989bc4bf91
Dongwoo Gim <gimdongwoo@gmail.com>
2018-03-07 16:17:54 +0900

    snapshot test

--- src/components/__tests__/AsideNav.spec.tsx
+++ src/components/__tests__/AsideNav.spec.tsx
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 
 import { AsideNav } from 'components';
 
-it('renders the span sqlgate.com', () => {
-  const result = shallow(<AsideNav />).contains(
-    <span className={'label'}>SQLGate.com</span>,
-  );
-  expect(result).toBeTruthy();
+describe('AsideNav', () => {
+  let component = null;
+
+  it('renders correctly', () => {
+    component = shallow(<AsideNav />);
+  });
+
+  it('matches snapshot', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders the span sqlgate.com', () => {
+    const result = shallow(<AsideNav />).contains(
+      <span className={'label'}>SQLGate.com</span>,
+    );
+    expect(result).toBeTruthy();
+  });
 });
