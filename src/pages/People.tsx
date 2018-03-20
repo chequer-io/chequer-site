8b6a82fe61bcaaecfd6a6a803998c381a5c23842
Thomas Jang <tom@axisj.com>
2018-03-20 14:00:43 +0900

    People component 정리

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -31,6 +31,7 @@ export default class People extends React.Component<
           },
         ],
       },
+      /*
       {
         id: 'john',
         name: 'John Song',
@@ -42,6 +43,7 @@ export default class People extends React.Component<
           },
         ],
       },
+      */
       {
         id: 'benjamin',
         name: 'Benjamin Yang',
