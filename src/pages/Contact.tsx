dd0050de4f167b8d2861235b01a496152d60e0ee
Kelly Kim <mein2555@gmail.com>
2018-05-21 17:33:57 +0900

    주소 변경

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -28,11 +28,11 @@ export default class Contact extends React.Component<
                 <div style={{ height: '300px' }}>
                   <GoogleMapViewer
                     text={'SEOUL OFFICE'}
-                    center={[37.5588997, 126.8621197]}
+                    center={[37.561919, 126.826919]}
                     zoom={11}
                     placeCoords={{
-                      lat: 37.5588997,
-                      lng: 126.8621197,
+                      lat: 37.561919,
+                      lng: 126.826919,
                     }}
                   />
                 </div>
@@ -43,8 +43,8 @@ export default class Contact extends React.Component<
                     &nbsp; SEOUL OFFICE
                   </Card.Header>
                   <Card.Description>
-                    1401 Hanhwa-BizMetro B-dong, 551-24, Yangcheon-ro,
-                    Gangseo-gu, Seoul, Republic of Korea
+                    #1116, 66, Magokjungang 6-ro, Gangseo-gu, Seoul, 07803, Rep.
+                    of KOREA
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
